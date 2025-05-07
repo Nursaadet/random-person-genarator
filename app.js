b2353734217be8dba471c61a821e1fd608b19844
Nursaadet <nursaadetozdemir@gmail.com>
2025-05-07 13:15:45 +0300

    developed the code

--- app.js
+++ app.js
@@ -6,34 +6,47 @@ const randomLastName = document.querySelector(".random-last");
 const mail = document.querySelector(".e-mail span");
 const adress = document.querySelector(".adress span");
 const phone = document.querySelector(".phone span");
-const button = document.querySelector("button");
-
+const button = document.querySelector("button")
+const body = document.querySelector("body");
 
 // Fetch
-fetch("https://randomuser.me/api/")
-  .then((response) => {
+
+const fetchData = async () => {
+  try {
+    const response = await fetch("https://randomuser.me/api/a");
+    console.log(response);
     if (!response.ok) {
       throw new Error("Something went wrong!");
     }
-    return response.json();
-  })
-  .then((data) => {
-    let result = data.results[0];
 
-    console.log(result);
+    const data = await response.json();
+    responseData(data);
+  } catch (error) {
+    errorHandling(error);
+  }
+};
 
-    img.src = result.picture.large;
-    randomTitle.textContent += `${result.name.title}.`;
-    randomName.textContent += result.name.first;
-    randomLastName.textContent += result.name.last;
-    mail.textContent += result.email;
-    adress.textContent += `${result.location.street.name} ${result.location.city} ${result.location.postcode} ${result.location.country}`;
-    phone.textContent += result.phone;
-  })
-  .catch((error) => {
-    console.error("Hata:", error);
-  });
+fetchData();
+
+const responseData = (data) => {
+  let result = data.results[0];
+
+  img.src = result.picture.large;
+  randomTitle.textContent += `${result.name.title}.`;
+  randomName.textContent += result.name.first;
+  randomLastName.textContent += result.name.last;
+  mail.textContent += result.email;
+  adress.textContent += `${result.location.street.name} ${result.location.city} ${result.location.postcode} ${result.location.country}`;
+  phone.textContent += result.phone;
+};
+
+// Error Handling
+const errorHandling = (error) => {
+  body.innerHTML = `<h3>${error}</h3>`;
+};
+    
 // Event
+
 button.addEventListener("click", () => {
   location.reload();
 });
