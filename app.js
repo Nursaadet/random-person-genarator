7a61cdc154f5cf7056460bdcfd126082cd7e1118
Nursaadet <nursaadetozdemir@gmail.com>
2025-05-07 11:24:01 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+fetch("https://randomuser.me/api/")
+  .then((response) => response.json())
+  .then((data) => {
+    let result = data.results[0];
+ 
+    console.log(result)
+
+  })
+  .catch((error) => {
+    console.error("Hata:", error);
+  });
